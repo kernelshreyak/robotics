a9c7230a6b1617d4ecf5d638dd756cf4e6c35d71
Shreyak Chakraborty <shreyak.rekshda@gmail.com>
2025-11-26 22:48:30 +0530

    added conveyor belt simulation in webots

---
+++ conveyor_belt/controllers/conveyor_controller/build/release/conveyor_controller.d
@@ -0,0 +1,6 @@
+build/release/conveyor_controller.o: conveyor_controller.c \
+ /usr/local/webots/include/controller/c/webots/robot.h \
+ /usr/local/webots/include/controller/c/webots/types.h \
+ /usr/local/webots/include/controller/c/webots/nodes.h \
+ /usr/local/webots/include/controller/c/webots/supervisor.h \
+ /usr/local/webots/include/controller/c/webots/contact_point.h
